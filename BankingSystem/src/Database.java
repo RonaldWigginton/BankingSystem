f5bf93b1034f4e64344c16bd8752a13a5626fea5
cstewart2 <35044627+cstewart2@users.noreply.github.com>
2021-04-29 09:58:51 -0500

    Update Database.java

--- BankingSystem/src/Database.java
+++ BankingSystem/src/Database.java
@@ -17,22 +17,18 @@ import Accounts.User;
 
 public class Database { // This is our Banking System Database
     // Load User Data
-    public static List<User> GetUserData(){
+    public static List<User> GetUserData() {
         // Changed for Windows bc not everyone uses linux bc linux is actually not that cool when you think about it.
         File file = new File("BankingSystem\\src\\Database\\customers.txt");
         var UserAccounts = new ArrayList<User>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
-            while((line = br.readLine()) != null)
-            {
+            while ((line = br.readLine()) != null) {
                 String[] chunks = line.split(",");
                 UserAccounts.add(new CustomerATM(Integer.parseInt(chunks[0]), chunks[1], chunks[2], chunks[3], Integer.parseInt(chunks[4]), chunks[5], chunks[6]));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -40,76 +36,66 @@ public class Database { // This is our Banking System Database
     }
 
     // Load Checking Acct Data
-    public static List<Account> GetCheckingData(){
+    public static List<Account> GetCheckingData() {
         File checkingFile = new File("BankingSystem\\src\\Database\\checkingAccounts.txt");
         var checkingAccounts = new ArrayList<Account>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(checkingFile)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(checkingFile))) {
             String line;
-            while((line = br.readLine()) != null)
-            {
+            while ((line = br.readLine()) != null) {
                 String[] chunks = line.split(",");
                 SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 
                 checkingAccounts.add(new CheckingAccount(Integer.parseInt(chunks[0]), Integer.parseInt(chunks[1]),
                         chunks[2], Double.parseDouble(chunks[3]), Integer.parseInt(chunks[4]), Integer.parseInt(chunks[5]),
-                        Integer.parseInt(chunks[6]),formatter.parse(chunks[7])));
+                        Integer.parseInt(chunks[6]), formatter.parse(chunks[7])));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return checkingAccounts;
     }
+
     // Load Loan Acct Data
-     public static  List<Account> GetLoanData(){
+    public static List<Account> GetLoanData() {
         File loanFile = new File("BankingSystem\\src\\Database\\loanAccounts.txt");
         var loanAccounts = new ArrayList<Account>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(loanFile)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(loanFile))) {
             String line;
-            while((line = br.readLine()) != null)
-            {
+            while ((line = br.readLine()) != null) {
                 String[] chunks = line.split(",");
                 SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 
-                loanAccounts.add(new LoanAccount(Integer.parseInt(chunks[0]),
-                        Double.parseDouble(chunks[1]), Double.parseDouble(chunks[2]), formatter.parse(chunks[3]),
-                        formatter.parse(chunks[4]), Double.parseDouble(chunks[5]), chunks[6],
-                        Integer.parseInt(chunks[7])==1, formatter.parse(chunks[8])));
+                loanAccounts.add(new LoanAccount(Integer.parseInt(chunks[0]), Integer.parseInt(chunks[1]),
+                        Double.parseDouble(chunks[2]), Double.parseDouble(chunks[3]), formatter.parse(chunks[4]),
+                        formatter.parse(chunks[5]), Double.parseDouble(chunks[6]), chunks[7],
+                        Integer.parseInt(chunks[8]) == 1, formatter.parse(chunks[9])));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return loanAccounts;
     }
+
     // Load Savings Actt Data
-     public static List<Account> GetSavingData(){
+    public static List<Account> GetSavingData() {
         File savingFile = new File("BankingSystem\\src\\Database\\savingsAccounts.txt");
         var savingAccounts = new ArrayList<Account>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(savingFile)))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader(savingFile))) {
             String line;
-            while((line = br.readLine()) != null)
-            {
+            while ((line = br.readLine()) != null) {
                 String[] chunks = line.split(",");
                 SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
 
                 savingAccounts.add(new SavingsAccount(Integer.parseInt(chunks[0]), Integer.parseInt(chunks[1]),
-                                                       Double.parseDouble(chunks[2]), Double.parseDouble(chunks[3]),
-                                                       formatter.parse(chunks[4])));
+                        Double.parseDouble(chunks[2]), Double.parseDouble(chunks[3]),
+                        formatter.parse(chunks[4]), chunks[5]));
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -117,71 +103,65 @@ public class Database { // This is our Banking System Database
     }
 
     // Save Customer Data
-    public static void SaveUserData(List<User> users)throws IOException {
+    public static void SaveUserData(List<User> users) throws IOException {
         File userFile = new File("BankingSystem\\src\\Database\\customers1.txt");
         FileWriter writer = new FileWriter(userFile);
         ArrayList<String[]> records = new ArrayList<String[]>();
 
-        for(var auser : users)
-        {
+        for (var auser : users) {
             records.add(auser.accountToArray());
         }
 
-        for(var record : records)
-        {
+        for (var record : records) {
             writer.append(String.join(",", record));
             writer.append("\n");
         }
 
         writer.flush();
-        writer.close(); 
+        writer.close();
     }
-// We can save whatever accounts 
- public static void SaveAccountData(List<Account> accounts)throws IOException {
-    File file = new File("");
-    System.out.println("Debug");
-    ArrayList<String[]> loanRecords = new ArrayList<String[]>();
-    ArrayList<String[]> savingRecords = new ArrayList<String[]>();
-    ArrayList<String[]> CheckingRecords = new ArrayList<String[]>();
-    for (Account account : accounts) {
-        if(account instanceof LoanAccount){
-            loanRecords.add(account.accountToArray());
-        }
-        else if (account instanceof SavingsAccount){
-            savingRecords.add(account.accountToArray());
-        }
-        else if (account instanceof CheckingAccount){
-            CheckingRecords.add(account.accountToArray());
-        }
-    }
-    file = new File("BankingSystem\\src\\Database\\loanAccounts1.txt");
-    FileWriter writer = new FileWriter(file);
 
-    for(var record : loanRecords)
-    {
-        writer.append(String.join(",",record));
-        writer.append("\n");
-    }
-    writer.flush();
-    writer.close();
+    // We can save whatever accounts
+    public static void SaveAccountData(List<Account> accounts) throws IOException {
+        File file = new File("");
+        System.out.println("Debug");
+        ArrayList<String[]> loanRecords = new ArrayList<String[]>();
+        ArrayList<String[]> savingRecords = new ArrayList<String[]>();
+        ArrayList<String[]> CheckingRecords = new ArrayList<String[]>();
+        for (Account account : accounts) {
+            if (account instanceof LoanAccount) {
+                loanRecords.add(account.accountToArray());
+            } else if (account instanceof SavingsAccount) {
+                savingRecords.add(account.accountToArray());
+            } else if (account instanceof CheckingAccount) {
+                CheckingRecords.add(account.accountToArray());
+            }
+        }
+        file = new File("BankingSystem\\src\\Database\\loanAccounts1.txt");
+        FileWriter writer = new FileWriter(file);
 
-    file = new File("BankingSystem\\src\\Database\\savingsAccounts1.txt");
-    writer = new FileWriter(file);
-
-    for(var record : savingRecords)
-    {
-        writer.append(String.join(",",record));
-        writer.append("\n");
-    }
+        for (var record : loanRecords) {
+            writer.append(String.join(",", record));
+            writer.append("\n");
+        }
         writer.flush();
         writer.close();
-        
+
+        file = new File("BankingSystem\\src\\Database\\savingsAccounts1.txt");
+        writer = new FileWriter(file);
+
+        for (var record : savingRecords) {
+            writer.append(String.join(",", record));
+            writer.append("\n");
+        }
+        writer.flush();
+        writer.close();
+
         file = new File("BankingSystem\\src\\Database\\checkingAccounts1.txt");
         writer = new FileWriter(file);
 
-        for(var record : CheckingRecords)
-        {
-            writer.append(String.join(",",record));
+        for (var record : CheckingRecords) {
+            writer.append(String.join(",", record));
             writer.append("\n");
         }
         writer.flush();
