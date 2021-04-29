c0bd9777ef4cbbf1cd4cd416ee62e4df8abcd7b2
cstewart2 <35044627+cstewart2@users.noreply.github.com>
2021-04-29 10:05:33 -0500

    Update SavingsAccount.java

--- BankingSystem/src/Accounts/SavingsAccount.java
+++ BankingSystem/src/Accounts/SavingsAccount.java
@@ -1,20 +1,30 @@
 package Accounts;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class SavingsAccount extends Account {
     private int accountNumber;
     private Date dateOpened;
+    private String isCD;
+    private Date CD;
 
     public SavingsAccount(int customerId, int accountNumber, double currentBalance,
-                          double interestRate, Date dateOpened)
+                          double interestRate, Date dateOpened, String isCD)
     {
         this.customerId = customerId;
         this.accountNumber = accountNumber;
         this.currentBalance = currentBalance;
         this.interestRate = interestRate;
         this.dateOpened=dateOpened;
+        this.isCD=isCD;
+        if(isCD.equals("yes")&& dateOpened.equals(new Date())){
+            Calendar date= Calendar.getInstance();
+            date.setTime(dateOpened);
+            date.add(Calendar.DATE,90);
+            this.CD= date.getTime();
+        }
         setStatus(0);
     }
     @Override
@@ -45,6 +55,28 @@ public class SavingsAccount extends Account {
         this.dateOpened = dateOpened;
     }
     public String getAccountType() {
-        return "Savings";
+        if(this.isCD.equals("no")){
+            return "Savings";
+        }
+        else{
+            return "CD";
+        }
+    }
+    @Override
+    public double withdraw(double amount) {
+        //withdraw money
+        if (currentBalance >= amount) {
+            currentBalance -= amount;
+        }
+        if(isCD.equals("yes")){
+            if(new Date().before(CD)){
+                Calendar date= Calendar.getInstance();
+                date.setTime(CD);
+                date.add(Calendar.DATE,90);
+                System.out.println("Customer: "+customerId+" has withdrawn money from their CD too early.");
+                CD = date.getTime();
+            }
+        }
+        return currentBalance;
     }
 }
